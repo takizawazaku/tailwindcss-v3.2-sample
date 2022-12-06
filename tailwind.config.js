c952629cc31aaa4ced695abb5b7efe39ca15483c
takizawazaku <takizawazaku@gmail.com>
2022-12-06 22:16:41 +0900

    ポートフォリオサイト作成用に初期化

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: "#ebf8ff",
-          300: "#90cdf4",
-          500: "#4299e1",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
