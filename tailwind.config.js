f81f76408326ba0c4ce95db7f816dc2234ce1db0
takizawazaku <takizawazaku@gmail.com>
2022-12-06 22:46:12 +0900

    カスタムカラーを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: '#050505',
+      },
+    },
     fontFamily: {
       poppins: ['Poppins', 'sans-serif'],
     },
