1095a340b25a91f44232aa6e306bd46c13d82398
takizawazaku <takizawazaku@gmail.com>
2022-12-11 17:18:30 +0900

    お問い合わせフォームセクションを作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         theme: '#5c318c',
         secondary: '#9191A4',
         badge: '#3F3F51',
+        inputBorder: '#565666',
+        input: '#2A2A33',
       },
     },
     fontFamily: {
